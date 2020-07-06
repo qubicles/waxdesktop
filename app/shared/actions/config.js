d7fe4bac2394a21e287ef2de653031f01e475ff8
Marlon Williams <marlonjwilliams@gmail.com>
2020-07-06 12:46:01 -0400

    First commit

---
+++ app/shared/actions/config.js
@@ -0,0 +1,7 @@
+export const CARBON_ROOT = process.env.NODE_ENV === "production" ? "https://api.carbon.money" : "https://sandbox.carbon.money";
+export const CARBON_TOKEN = process.env.NODE_ENV === "production" ? 'gdfgfdgfdgdf' : 'dfssdfsdfdsfs';
+export const PAY_FOR_CPU_KEY = 'dfsdfsdfsdf';
+export const PRICE_API_SECRET = 'dfsdfsdfd';
+export const RB_API_KEY = 'sfgfgdsfgfd';
+export const RB_WORKSPACE_ID = 'gfdgdfgfdgdf';
+export const SQRL_CONTRACT = 'sqrlwalletio';
