47417cf576a370cbbc00ea6363ac1878ffe0ffa1
vlad <favdev@favs-iMac-Pro.local>
2020-08-14 15:17:42 +0300

    add balance section after rebasing

--- app/shared/components/v2/Dashboard/Dashboard.js
+++ app/shared/components/v2/Dashboard/Dashboard.js
@@ -148,6 +148,58 @@ class Home extends React.Component {
 					openResourcesModal={this.toggleResourcesModal}
 					openDelegateModal={this.toggleDelegateModal}	 
 				/>
+				<div className="balance-section">
+					<div className="balance-chart-container">
+						<div className="balance-total">
+							<h4>Total Balance</h4>
+							<div className="balance-percent">
+								+3.49%
+							</div>
+						</div>
+						<h2>$12,184.62</h2>
+						<div className="chart-img">
+							<img src={require('../../../../renderer/assets/images/dashboard/Group1733.png')} />
+						</div>
+						<div className="chart-button-group">
+							<div className="chart-white-btn">1D</div>
+							<div className="chart-white-btn">1W</div>
+							<div className="chart-white-btn">1M</div>
+							<div className="chart-orange-btn">All</div>
+						</div>
+						<div className="send-btn-wrap">
+							<div className="dashboard-send-btn">
+								<h3>Send</h3>
+								<img src={require('../../../../renderer/assets/images/dashboard/arrow1.png')} />
+							</div>
+						</div>
+						<div className="send-btn-wrap">
+							<div className="dashboard-send-btn" onClick={this.toggleDashboardTokenModal}>
+								<h3>Receive</h3>
+								<img src={require('../../../../renderer/assets/images/dashboard/iconfinder263.png')} />
+							</div>
+						</div>
+					</div>
+					<div className="balance-button-group">
+						<div className="balance-button-wrap">
+							<div className="balance-button-title">
+								Buy WAX
+							</div>
+							<img src={require('../../../../renderer/assets/images/dashboard/credit-card2.png')} />
+						</div>
+						<div className="balance-button-wrap">
+							<div className="balance-button-title" onClick={this.toggleSwapTokenModal}>
+								Swap Tokens
+							</div>
+							<img src={require('../../../../renderer/assets/images/dashboard/Group1734.png')} />
+						</div>
+						<div className="balance-button-wrap">
+							<div className="balance-button-title" onClick={this.toggleResourcesModal}>
+								Resources
+							</div>
+							<img src={require('../../../../renderer/assets/images/dashboard/Group15.png')} />
+						</div>
+					</div>
+				</div>
 				<DashboardTokenModal
 					closeModal={this.toggleDashboardTokenModal}
 					modalOpen={dashboardTokenModal}
