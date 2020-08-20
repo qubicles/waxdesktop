59e7d8320c8a7be8285015ef622552e09f8b5081
Rakesh Sagitra <rksgprog@gmail.com>
2020-08-20 18:56:04 +0530

    Worked on the tokens balance tab Displayed the core token with currency balance and USD balance Displayed the total balance in UI

--- app/shared/components/v2/Dashboard/TabPanes/TabPanes.js
+++ app/shared/components/v2/Dashboard/TabPanes/TabPanes.js
@@ -1,34 +1,19 @@
 import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import { Tab, Table, Image, Header } from "semantic-ui-react";
+import { forEach } from 'lodash';
 
-import './TabPanes.global.css'
+import "./TabPanes.global.css";
 
 class TabPanes extends React.Component {
   getPanes() {
-    const { tokens } = this.props;
     const tokensPane = {
       menuItem: "Tokens",
       render: () => (
         <Tab.Pane attached={false}>
           <Table className="tokens-table">
-            <Table.Body>
-              {tokens.map((token, id) => (
-                <Table.Row className="token-wrap" key={id}>
-                  <Table.Cell>
-                    <Image
-                      src={require(`../../../../../renderer/assets/images/dashboard/${token.img}`)}
-                    />
-                  </Table.Cell>
-                    <Table.Cell className="token-des">
-                      <div className="des-title">
-                        <Header as="h3">{token.name}</Header>
-                        <Header as="h5">{token.quant}</Header>
-                      </div>
-                    </Table.Cell>
-                    <Table.Cell className="des-price">{token.price}</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
+            <TokenBalance {...this.props} />
           </Table>
         </Tab.Pane>
       )
@@ -52,4 +37,83 @@ class TabPanes extends React.Component {
   }
 }
 
-export default TabPanes;
+class TokenBalance extends React.Component {
+  render() {
+    const { settings, globals, balances, statsFetcher } = this.props;
+    const { tokens, totalTokens } = statsFetcher.fetchAll();
+
+    let coreTokenInfo = globals.remotetokens && globals.remotetokens.filter((t) => t.symbol == settings.blockchain.tokenSymbol)[0];
+    let coreTokenName = settings.blockchain.tokenSymbol;
+    if (coreTokenInfo)
+      coreTokenName = coreTokenInfo.name;
+    else
+      coreTokenInfo = { logo: null };
+
+    const coreTokenBalance = tokens[settings.account] ? tokens[settings.account][settings.blockchain.tokenSymbol] : 0;
+    const coreTokenUSDBalance = 1 * (globals.pricefeed ? globals.pricefeed.CUSD.price : 0);
+
+    const rows = [
+      (
+        <Table.Row className="token-wrap" key={settings.blockchain.tokenSymbol}>
+          <Table.Cell>
+            <Image
+              src={coreTokenInfo.logo}
+            />
+          </Table.Cell>
+          <Table.Cell className="token-des">
+            <div className="des-title">
+              <Header as="h3">{coreTokenName}</Header>
+              <Header as="h5">{coreTokenBalance.toFixed(4)}</Header>
+            </div>
+          </Table.Cell>
+          <Table.Cell className="des-price">${coreTokenUSDBalance.toFixed(4)}</Table.Cell>
+        </Table.Row>
+      )];
+
+    const watchedTokens = (settings.customTokens) ? settings.customTokens.map((token) => token.split(':')[1]) : [];
+
+    forEach(tokens[settings.account], (amount, token) => {
+      if (token.toUpperCase() === settings.blockchain.tokenSymbol || watchedTokens.indexOf(token) === -1 || amount < 0.0002) return;
+      let tokenInfo = globals.remotetokens && globals.remotetokens.filter((t) => t.symbol == token && t.chain.toUpperCase() == settings.blockchain.tokenSymbol)[0];
+
+      let tokenName = token;
+      let contract = 'unknown';
+      let precision = {
+        [token]: 4
+      };
+      if (tokenInfo)
+        tokenName = tokenInfo.name;
+      else
+        tokenInfo = { logo: null };
+
+      rows.push((<Table.Row className="token-wrap" key={token}>
+        <Table.Cell>
+          <Image
+            src={tokenInfo.logo}
+          />
+        </Table.Cell>
+        <Table.Cell className="token-des">
+          <div className="des-title">
+            <Header as="h3">{tokenName}</Header>
+            <Header as="h5">{amount}</Header>
+          </div>
+        </Table.Cell>
+        <Table.Cell className="des-price"></Table.Cell>
+      </Table.Row>))
+
+    })
+    return <Table.Body>
+      {rows}
+    </Table.Body>
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    balances: state.balances,
+    settings: state.settings,
+    globals: state.globals
+  };
+}
+
+export default connect(mapStateToProps, null)(TabPanes);
