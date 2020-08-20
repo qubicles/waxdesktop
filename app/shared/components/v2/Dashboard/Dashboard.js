adc8c72e821ced6f967ac212e8ad5f0e1053616f
fav dev <favdev@favs-iMac-Pro.local>
2020-08-20 05:01:17 +0300

    finish Guilds page

--- app/shared/components/v2/Dashboard/Dashboard.js
+++ app/shared/components/v2/Dashboard/Dashboard.js
@@ -128,7 +128,7 @@ class Home extends React.Component {
 		this.props.history.push("/apps")
 	}
 	goStaking = () => {
-		this.props.history.push("/staking")
+		this.props.history.push("/guilds")
 	}
 	
 	render() {
