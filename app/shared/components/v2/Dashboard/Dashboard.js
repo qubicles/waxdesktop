4ee82a22ca42075b0bc28233aa231526189195a5
fav dev <favdev@favs-iMac-Pro.local>
2020-08-21 08:07:37 +0300

    finish design for advanced-walletSettings

--- app/shared/components/v2/Dashboard/Dashboard.js
+++ app/shared/components/v2/Dashboard/Dashboard.js
@@ -158,7 +158,7 @@ class Home extends React.Component {
 		this.props.history.push("/advanced")
 	}
 	goStaking = () => {
-		this.props.history.push("/guilds")
+		this.props.history.push("/advanced")
 	}
 	
 	render() {
