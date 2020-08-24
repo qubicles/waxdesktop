11ea2d5f4e1176da983f9f618153f47fc5bb3a4c
fav dev <favdev@favs-iMac-Pro.local>
2020-08-24 09:00:34 +0300

    finish WhiteListed apps page

--- app/shared/components/v2/Advanced/WhiteListedApps/WhiteListedApps.js
+++ app/shared/components/v2/Advanced/WhiteListedApps/WhiteListedApps.js
@@ -91,9 +91,12 @@ class WhiteListedApps extends React.Component {
 	}
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> d0a8795b... build architechure for whitelisted apps page
+=======
+>>>>>>> 8f7638a9... finish WhiteListed apps page
 WhiteListedApps.propTypes = {
 
 }
