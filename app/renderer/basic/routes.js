88a9d9f92e25101154d7d2089b95363dbb44f394
fav dev <favdev@favs-iMac-Pro.local>
2020-08-25 00:38:40 +0300

    Finish Advanced-smartContractAccount page

--- app/renderer/basic/routes.js
+++ app/renderer/basic/routes.js
@@ -21,7 +21,10 @@ import WhiteListedApps from '../../shared/components/v2/Advanced/WhiteListedApps
 import DelegatedResources from '../../shared/components/v2/Advanced/DelegatedResources/DelegatedResources'
 import SmartContractAccount from '../../shared/components/v2/Advanced/SmartContract/SmartContractAccount/SmartContractAccount'
 import SmartContractActions from '../../shared/components/v2/Advanced/SmartContract/SmartContractActions/SmartContractActions'
+<<<<<<< HEAD
 import ConnectLedger from '../../shared/components/v2/Advanced/ConnectLedger/ConnectLedger'
+=======
+>>>>>>> e5b2b0b7... Finish Advanced-smartContractAccount page
 import { withSideBar } from '../../shared/components/v2/hoc/withSideBar'
 
 export default () => (
@@ -40,7 +43,10 @@ export default () => (
       <Route exact path="/delegatedResources" component={withSideBar(DelegatedResources)} />
       <Route exact path="/smartContractAccount" component={withSideBar(SmartContractAccount)} />
       <Route exact path="/smartContractActions" component={withSideBar(SmartContractActions)} />
+<<<<<<< HEAD
       <Route exact path="/connectLedger" component={withSideBar(ConnectLedger)} />
+=======
+>>>>>>> e5b2b0b7... Finish Advanced-smartContractAccount page
       <Route exact path="/coldwallet" component={ColdWallet} />
       <Route exact path="/voter" component={BasicVoter} />
     </Switch>
