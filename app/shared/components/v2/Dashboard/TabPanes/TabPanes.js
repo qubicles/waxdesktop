81489d4e273a86d09d68f074ea099059c127501e
Rakesh Sagitra <rksgprog@gmail.com>
2020-08-28 20:44:37 +0530

    Refactored the code

--- app/shared/components/v2/Dashboard/TabPanes/TabPanes.js
+++ app/shared/components/v2/Dashboard/TabPanes/TabPanes.js
@@ -74,10 +74,10 @@ class TokenBalance extends React.Component {
           <Table.Cell className="token-des">
             <div className="des-title">
               <Header as="h3">{coreTokenName}</Header>
-              <Header as="h5">{coreTokenBalance.toFixed(4)}</Header>
+              <Header as="h5">{coreTokenBalance && coreTokenBalance.toFixed(4)}</Header>
             </div>
           </Table.Cell>
-          <Table.Cell className="des-price">${coreTokenUSDBalance.toFixed(2)}</Table.Cell>
+          <Table.Cell className="des-price">${coreTokenUSDBalance && coreTokenUSDBalance.toFixed(2)}</Table.Cell>
         </Table.Row>
       )];
 
