fb2f480bac119832450e911098acc01ee2606314
fav dev <favdev@favs-iMac-Pro.local>
2020-08-28 09:47:57 +0300

    fix font issue and create new component for apps page

--- app/shared/components/v2/Apps/Apps.js
+++ app/shared/components/v2/Apps/Apps.js
@@ -5,6 +5,8 @@ import { Card, Image, Divider, Tab, Button, Dropdown } from "semantic-ui-react"
 import { connect } from 'react-redux';
 import Balance from "../Dashboard/Balance/Balance"
 import "./Apps.global.css"
+import RecentlyApps from "./RecentlyApps/RecentlyApps"
+import DecentralizedApps from "./DecentralizedApps/DecentralizedApps"
 
 
 class Apps extends React.Component {
@@ -18,84 +20,10 @@ class Apps extends React.Component {
 		return (
 			<div className="dashboard-container">
 				<div className="dashboard-body-section">
-                    <div className="recommended-apps-section">
-                        <div className="recommended-apps-header">
-                            <div className="recommended-apps-title">Recently Used</div>
-                            <img src={require('../../../../renderer/assets/images/dashboard/Group1737.png')} />
-                        </div>
-                        <div className="recommended-apps-body">
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup2.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup4.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/Group209.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-                            <div className="recommended-apps-card">
-                                <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                <div className="app-title">KARMAR</div>
-                            </div>
-
-                            
-                        </div>
-                    </div>
-                    <div className="decentralized-apps-section">
-                        <div className="decentralized-apps-container">
-                            <div className="decentralized-apps-header">Decentralized Applications</div>
-                            <div className="decentralized-apps-body">
-                                <div className="dec-left-section">
-                                    <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                    <div className="dec-des">
-                                        <div>Newdex</div>
-                                        <div>Decentralized Exchange</div>
-                                    </div>
-                                </div>
-                                <div className="dec-right-section">
-                                    Open
-                                </div>
-                            </div>
-                            <div className="decentralized-apps-body">
-                                <div className="dec-left-section">
-                                    <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                    <div className="dec-des">
-                                        <div>Newdex</div>
-                                        <div>Decentralized Exchange</div>
-                                    </div>
-                                </div>
-                                <div className="dec-right-section">
-                                    Open
-                                </div>
-                            </div>
-                            <div className="decentralized-apps-body">
-                                <div className="dec-left-section">
-                                    <img src={require('../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                                    <div className="dec-des">
-                                        <div>Newdex</div>
-                                        <div>Decentralized Exchange</div>
-                                    </div>
-                                </div>
-                                <div className="dec-right-section">
-                                    Open
-                                </div>
-                            </div>
-                        </div>
-                        
-                    </div>
+                    <RecentlyApps />
+                    <DecentralizedApps />
                 </div>
-                <Balance />
+                {/* <Balance /> */}
             </div>
 
 		)
