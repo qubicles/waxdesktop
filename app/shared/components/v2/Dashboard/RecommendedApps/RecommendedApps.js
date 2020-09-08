ad749da3623670f6339112166dbea26d622323b0
fav dev <favdev@favs-iMac-Pro.local>
2020-09-08 13:11:44 +0300

    fix font issue and change font on dashboard

--- app/shared/components/v2/Dashboard/RecommendedApps/RecommendedApps.js
+++ app/shared/components/v2/Dashboard/RecommendedApps/RecommendedApps.js
@@ -1,26 +1,29 @@
 import React from "react";
+import "./RecommendedApps.global.css";
 
-import recommendedAppsJSON from "../../../../../../resources/recommendedApps.json"
+import recommendedAppsJSON from "../../../../../../resources/recommendedApps.json";
 
 // import "./RecommendedApps.global.css";
 
 export default () => {
-    const recommendedApps = recommendedAppsJSON.map((app, index) => (
-        <div className="recommended-apps-card" key={`recommeded-${index}`}>
-            <img src={require(`../../../../../renderer/assets/images/dashboard/${app.image}`)} />
-            <div className="app-title">{app.name}</div>
-        </div>
-    ));
+  const recommendedApps = recommendedAppsJSON.map((app, index) => (
+    <div className="recommended-apps-card" key={`recommeded-${index}`}>
+      <img
+        src={require(`../../../../../renderer/assets/images/dashboard/${app.image}`)}
+      />
+      <div className="app-title">{app.name}</div>
+    </div>
+  ));
 
-    return (
-        <div className="recommended-apps-section">
-            <div className="recommended-apps-header">
-                <div className="recommended-apps-title">Recommended Apps</div>
-                <img src={require('../../../../../renderer/assets/images/dashboard/Group1737.png')} />
-            </div>
-            <div className="recommended-apps-body">
-                {recommendedApps}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="recommended-apps-section">
+      <div className="recommended-apps-header">
+        <div className="recommended-apps-title">Recommended Apps</div>
+        <img
+          src={require("../../../../../renderer/assets/images/dashboard/Group1737.png")}
+        />
+      </div>
+      <div className="recommended-apps-body">{recommendedApps}</div>
+    </div>
+  );
+};
