4d48d901eec8c53ba260af9e1e3be56c44122056
fav dev <favdev@favs-iMac-Pro.local>
2020-09-10 14:44:49 +0300

    Sort App page design

--- app/shared/components/v2/Apps/RecentlyApps/RecentlyApps.js
+++ app/shared/components/v2/Apps/RecentlyApps/RecentlyApps.js
@@ -3,34 +3,46 @@ import React from "react";
 // import "./RecommendedApps.global.css";
 
 export default () => {
-    return (
-        <div className="recommended-apps-section">
-            <div className="recommended-apps-header">
-                <div className="recommended-apps-title">Recommended Apps</div>
-                <img src={require('../../../../../renderer/assets/images/dashboard/Group1737.png')} />
-            </div>
-            <div className="recommended-apps-body">
-                <div className="recommended-apps-card">
-                    <img src={require('../../../../../renderer/assets/images/dashboard/ScrollGroup1.png')} />
-                    <div className="app-title">KARMAR</div>
-                </div>
-                <div className="recommended-apps-card">
-                    <img src={require('../../../../../renderer/assets/images/dashboard/ScrollGroup2.png')} />
-                    <div className="app-title">Newdex</div>
-                </div>
-                <div className="recommended-apps-card">
-                    <img src={require('../../../../../renderer/assets/images/dashboard/ScrollGroup4.png')} />
-                    <div className="app-title">KARMAR</div>
-                </div>
-                <div className="recommended-apps-card">
-                    <img src={require('../../../../../renderer/assets/images/dashboard/ScrollGroup5.png')} />
-                    <div className="app-title">KARMAR</div>
-                </div>
-                <div className="recommended-apps-card">
-                    <img src={require('../../../../../renderer/assets/images/dashboard/Group209.png')} />
-                    <div className="app-title">KARMAR</div>
-                </div>
-            </div>
+  return (
+    <div className="recommended-apps-section">
+      <div className="recommended-apps-header">
+        <div className="recommended-apps-title">Recently Used</div>
+        <img
+          src={require("../../../../../renderer/assets/images/dashboard/Group1737.png")}
+        />
+      </div>
+      <div className="recommended-apps-body">
+        <div className="recommended-apps-card">
+          <img
+            src={require("../../../../../renderer/assets/images/dashboard/ScrollGroup1.png")}
+          />
+          <div className="app-title">KARMAR</div>
         </div>
-    )
-}
+        <div className="recommended-apps-card">
+          <img
+            src={require("../../../../../renderer/assets/images/dashboard/ScrollGroup2.png")}
+          />
+          <div className="app-title">Newdex</div>
+        </div>
+        <div className="recommended-apps-card">
+          <img
+            src={require("../../../../../renderer/assets/images/dashboard/ScrollGroup4.png")}
+          />
+          <div className="app-title">KARMAR</div>
+        </div>
+        <div className="recommended-apps-card">
+          <img
+            src={require("../../../../../renderer/assets/images/dashboard/ScrollGroup5.png")}
+          />
+          <div className="app-title">KARMAR</div>
+        </div>
+        <div className="recommended-apps-card">
+          <img
+            src={require("../../../../../renderer/assets/images/dashboard/Group209.png")}
+          />
+          <div className="app-title">KARMAR</div>
+        </div>
+      </div>
+    </div>
+  );
+};
