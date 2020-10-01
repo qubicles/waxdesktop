bf48593ed295f1c2935dc94964e04b17c473b011
fav dev <favdev@favs-iMac-Pro.local>
2020-10-01 13:39:06 +0300

    create SellAssets modal

--- app/shared/components/v2/Dashboard/Modals/SellAssetModal/SellAssetModal.js
+++ app/shared/components/v2/Dashboard/Modals/SellAssetModal/SellAssetModal.js
@@ -2,12 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Dropdown, Modal } from "semantic-ui-react";
 
-// import "./SellAssetModal.global.css"
+import "./sellAssetModal.global.css";
+
+const Styles = {
+  elementHidden: { display: "none" },
+  elementShow: { color: "block" }
+};
 
 class SellAssetModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { changeInput: true };
+  }
+
+  changeSellerInput() {
+    if (event.target.value != "") {
+      this.setState({ changeInput: false });
+    } else {
+      this.setState({ changeInput: true });
+    }
   }
 
   render() {
@@ -30,7 +43,7 @@ class SellAssetModal extends React.Component {
                 <div className="sc-brand">theonlykarma</div>
                 <div className="sc-price">
                   <img
-                    src={require("../../../../../../renderer/assets/images/dashboard/Group47.png")}
+                    src={require("../../../../../../renderer/assets/images/dashboard/Group1684.png")}
                   />
                   <div>25,000 KARMA</div>
                 </div>
@@ -38,37 +51,65 @@ class SellAssetModal extends React.Component {
             </div>
           </div>
           <div className="sell-right-container">
-            <div className="sellModal-title">
-              <span>Sell </span>
-              <span> Assets</span>
-            </div>
-            <div className="seller-input">
-              <div className="input-title">Seller</div>
-              <input
-                type="text"
-                className="common-input"
-                placeholder="yogi3.wam"
-              />
-            </div>
-            <h3>Breakdown</h3>
-            <div className="seller-info">
-              <span>Seller Gets</span>
-              <span>1,820 WAX</span>
-            </div>
-            <div className="seller-info author-fee">
-              <span>Author Fee</span>
-              <span>8%</span>
-              <span>160 WAX</span>
-            </div>
-            <div className="seller-info wdw-fee">
-              <span>WDW Fee</span>
-              <span>1%</span>
-              <span>20 WAX</span>
-            </div>
-            <div className="seller-info total-fee">
-              <span>Total</span>
-              <span>2,000 WAX</span>
-              <span>$100.00</span>
+            <div>
+              <div className="sellModal-title">
+                <span>Sell </span>
+                <span> Assets</span>
+              </div>
+              <div className="seller-input first-element">
+                <div className="input-title">Instant Buy Price</div>
+                <input
+                  type="text"
+                  className="common-input"
+                  placeholder="yogi3.wam"
+                />
+              </div>
+              <div className="seller-input">
+                <div className="input-title">Cheapest Market Price</div>
+                <input
+                  type="text"
+                  className="common-input multi-placeholder"
+                  onChange={() => this.changeSellerInput()}
+                />
+                <div
+                  style={
+                    this.state.changeInput
+                      ? Styles.elementShow
+                      : Styles.elementHidden
+                  }
+                >
+                  2,115 WAX
+                </div>
+                <div
+                  style={
+                    this.state.changeInput
+                      ? Styles.elementShow
+                      : Styles.elementHidden
+                  }
+                >
+                  $116.32
+                </div>
+              </div>
+              <h3>Breakdown</h3>
+              <div className="seller-info">
+                <span>List Price</span>
+                <span>2000 WAX</span>
+              </div>
+              <div className="seller-info author-fee">
+                <span>Author Fee</span>
+                <span>8%</span>
+                <span>160 WAX</span>
+              </div>
+              <div className="seller-info wdw-fee">
+                <span>WDW Fee</span>
+                <span>1%</span>
+                <span>20 WAX</span>
+              </div>
+              <div className="seller-info total-fee">
+                <span>Total</span>
+                <span>1820 WAX</span>
+                <span>$100.00</span>
+              </div>
             </div>
             <div className="delegate-btn">
               Confirm Purchase
