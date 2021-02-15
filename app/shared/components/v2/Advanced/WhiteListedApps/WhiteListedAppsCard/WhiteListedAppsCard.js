96ec452b2ef7792a0265a1e31c7ff08a46f40ea4
zenblocks <67918302+zenblocks@users.noreply.github.com>
2021-02-15 08:53:21 -0500

    finishing UI Design for Whitelisted apps

--- app/shared/components/v2/Advanced/WhiteListedApps/WhiteListedAppsCard/WhiteListedAppsCard.js
+++ app/shared/components/v2/Advanced/WhiteListedApps/WhiteListedAppsCard/WhiteListedAppsCard.js
@@ -1,29 +1,127 @@
 import React from "react";
 import SlideToggle from "react-slide-toggle";
 import PropTypes from "prop-types";
+import { translate } from 'react-i18next';
+import { debounce, filter, sortBy, find } from 'lodash';
 import {
   Card,
-  Image,
-  Divider,
-  Tab,
   Button,
-  Dropdown,
-  Radio,
-  Menu,
-  Checkbox
-} from "semantic-ui-react";
+  Confirm,
+  Container,
+  Icon,
+  Header, 
+  Input,
+  Message,
+  Segment,
+  Table
+} from 'semantic-ui-react';
 import { connect } from "react-redux";
 import Balance from "../../../Dashboard/Balance/Balance";
 import "./WhiteListedAppsCard.global.css";
+import AuthorizedApp from '../../../../../../wallet-integration/API/models/AuthorizedApp'
+import {PermissionList} from'../../../../../../wallet-integration/API/models/PermissionList'
+import APIUtils from '../../../../../../wallet-integration/API/util/APIUtils'
+
+
 
 class WhiteListedAppsCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
+    constructor(props) {
+      super(props)
+      this.state = {
+        query: '',
+        numberToLoad: 10,
+        confirmDeleteAll: false,
+        confirmDeleteApp: false,
+        confirmDeletePermission: false,
+        appToDelete: undefined,
+        permissionToDelete: undefined
+      };
+    }
+  
+    onSearchChange = debounce((e, { value }) => {
+      const query = String(value).toLowerCase();
+  
+      this.setState({ query });
+    }, 300);
+  
+    render() {
+      const {
+        accounts,
+        actions,
+        blockExplorers,
+        keys,
+        settings,
+        system,
+        t,
+        validate,
+        wallet,
+        wapii,
+        connection
+      } = this.props;
+  
+      const account = accounts[settings.account];
+  
+      if (!account || !wapii || !wapii.authorizedApps || !wapii.authorizedApps.length)
+        return (
+          <Message
+            header='Whitelisted Apps'
+            content='There are no apps connected to your Wax Desktop Wallet'
+            icon="info circle"
+            info
+          />
+        );
+  
+      const {
+        confirmDeleteAll,
+        confirmDeleteApp,
+        confirmDeletePermission,
+        numberToLoad,
+        query
+      } = this.state;
+  
+      const {
+        removeApp,
+        removeApps,
+        removePermission,
+        removePermissionsByOrigin
+      } = actions;
+  
+      const apps = wapii.authorizedApps.map((app) => {
+        return new AuthorizedApp(app.origin, app.appkey, app.nextNonce, app.createdAt);
+      });
+  
+      const sortedApps = sortBy(apps, 'origin');
+  
+      const appsToDisplay = filter(sortedApps, (app) => {
+        const matchesLabel = (String(app.origin).toLowerCase()).indexOf(query) > -1;
+  
+        return !query || matchesLabel;
+      }).slice(0, numberToLoad);
+  
+      let permissions = PermissionList.permissionsPerOrigin(wapii.permissions);
+      permissions = Object.keys(permissions)
+        .reduce((value, origin) => {
+          value[origin] = PermissionList.permissionsPerType( permissions[origin] );
+          return value;
+        }, {});
+
     return (
       <div className="item-list-wrap">
+        <Segment basic>
+          <Input
+            icon="search"
+            onChange={this.onSearchChange}
+            placeholder={t('tools_permissions_app_text')}
+            floated="left"
+          />
+          <Button
+            content="Delete All"
+            icon="trash alternate"
+            floated="right"
+            onClick={() => this.setState({ confirmDeleteAll: true})}>
+          </Button>
+        </Segment>
+        {appsToDisplay.map((app, i) => (
         <SlideToggle
           collapsed
           duration={800}
@@ -31,13 +129,6 @@ class WhiteListedAppsCard extends React.Component {
             <div className="item-list-body">
               <div className="item-parent-wrap">
                 <div className="item-parent-left" onClick={onToggle}>
-                  <img
-                    src={require("../../../../../../renderer/assets/images/dashboard/ScrollGroup1.png")}
-                  />
-                  <div className="w-des">
-                    <div>Newdex</div>
-                    <div>3 Type Of Transactions</div>
-                  </div>
                 </div>
                 <div className="item-parent-right">
                   <div className="round-checkbox">
@@ -45,6 +136,8 @@ class WhiteListedAppsCard extends React.Component {
                   </div>
                 </div>
               </div>
+
+
               <div className="item-child-wrap" ref={setCollapsibleElement}>
                 <div
                   className="item-child-inner"
@@ -54,20 +147,25 @@ class WhiteListedAppsCard extends React.Component {
                     )}px)`
                   }}
                 >
+
                   <div className="item-child-group">
-                    <div className="item-child-des">Marketbuy</div>
+                    <div className="item-child-des"></div>
                     <div className="common-checkbox">
                       <Checkbox />
                     </div>
                   </div>
+
+
                   <div className="item-child-group">
-                    <div className="item-child-des">Marketsell</div>
+                    <div className="item-child-des"></div>
                     <div className="common-checkbox">
                       <Checkbox />
                     </div>
                   </div>
+
+
                   <div className="item-child-group">
-                    <div className="item-child-des">Limitorder</div>
+                    <div className="item-child-des"></div>
                     <div className="common-checkbox">
                       <Checkbox />
                     </div>
@@ -77,6 +175,7 @@ class WhiteListedAppsCard extends React.Component {
             </div>
           )}
         />
+        ))}
       </div>
     );
   }
